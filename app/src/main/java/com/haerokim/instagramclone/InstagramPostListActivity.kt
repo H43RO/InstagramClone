325615f567efb417a426fa80c5003d0a08279b92
H43RO <h2is1234@naver.com>
2020-04-07 15:21:34 +0900

    Commit test with android studio

--- app/src/main/java/com/haerokim/instagramclone/InstagramPostListActivity.kt
+++ app/src/main/java/com/haerokim/instagramclone/InstagramPostListActivity.kt
@@ -1,14 +1,12 @@
 package com.haerokim.instagramclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -17,8 +15,6 @@ import kotlinx.android.synthetic.main.activity_instagram_post_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.POST
-import java.util.zip.Inflater
 
 class InstagramPostListActivity : AppCompatActivity() {
 
