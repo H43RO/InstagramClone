a5bf1829f6250f5f077f69120c23adba2d4f567e
H43RO <h2is1234@naver.com>
2020-04-09 17:57:37 +0900

    Implement Post Upload

--- app/src/main/java/com/haerokim/instagramclone/PostUploadActivity.kt
+++ app/src/main/java/com/haerokim/instagramclone/PostUploadActivity.kt
@@ -1,9 +1,12 @@
 package com.haerokim.instagramclone
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_instagram_post_list.*
+import kotlinx.android.synthetic.main.activity_post_upload.*
 
 class PostUploadActivity : AppCompatActivity() {
 
@@ -11,17 +14,36 @@ class PostUploadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_upload)
 
-        all_feed.setOnClickListener {
-            startActivity(Intent(this, InstagramPostListActivity::class.java))
+        view_pictures.setOnClickListener {
+            getPicture()
         }
 
+    }
 
-        user_info.setOnClickListener {
-            startActivity(Intent(this, UserInfo::class.java))
-        }
+    fun getPicture() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setType("image/*")
+        startActivityForResult(intent, 1000)
+    }
 
-        my_feed.setOnClickListener {
-            startActivity(Intent(this, MyFeedList::class.java))
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val uri: Uri = data!!.data!! //선택한 이미지의 위치를 찾아줌 (상대경로)
+            var a = getImageFilePath(uri)
+            Log.d("pathh","path = "+a)
+
         }
+    }
+
+    fun getImageFilePath(contentUri:Uri):String{
+        var columnIndex = 0
+        val projection = arrayOf(MediaStore.Images.Media.DATA) //필터링
+        val cursor = contentResolver.query(contentUri,projection,null,null,null) //절대경로 얻는 인덱스
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA) //cursor
+        }
+        return cursor.getString(columnIndex) //cursor에 해당하는 이미지의 절대경로 나옴
     }
 }
